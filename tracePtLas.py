c310f307f519fb9fd83a12c59bb122c7f8cc5881
Fraiddd <frederick.coulon@gmail.com>
2023-02-27 10:27:14 +0100

    v1.0

--- tracePtLas.py
+++ tracePtLas.py
@@ -1,3 +1,4 @@
+
 # coding: utf-8
 # Python 3.11.1
 '''
@@ -43,52 +44,57 @@ def tracePtLas(acad):
         # Loop over files
         for fil in file_path:
             cptt = 0
-            loc = 33
+            sens = 33
+            step = 0
             time.sleep(2)
             doc.SendCommand('(vla-open (vla-get-documents (vlax-get-acad-object)) "c:/Data/Python/tracePtLas/dwg/Lambert93.dwg")\n')
             time.sleep(4)
             doc.SendCommand('(vla-activate (vla-item (vla-get-documents (vlax-get-acad-object)) "Lambert93.dwg"))\n')
             time.sleep(3)
+            # Reconnect to Autocad in the new draw.
             acad = Autocad()            
             doc = acad.ActiveDocument
-            # Las reading
+            # Las reading.
             las = laspy.read(fil)
             # number of points
             ptCount = las.header.point_count
+            # We ignore files containing less than 1000 points.
             if ptCount > 1000:
                 # Get Coordinates
                 lx = las.X
                 ly = las.Y
-                # Get scle and offset to convert coordinates
+                # Get scale and offset to convert coordinates.
                 xscale = las.header.scales[0]
                 xoffset = las.header.offsets[0]
                 yscale = las.header.scales[1]
                 yoffset = las.header.offsets[1]
-                # loop over points
+                # loop over points.
                 while cptt < ptCount:
-                    if loc == 33:
+                    if sens == 33:
                         acad.model.AddPoint(APoint((lx[cptt] * xscale)+xoffset,
                                         (ly[cptt] * yscale) + yoffset, 0.0))
-                        loc = 0
+                        sens = 0
+                        step += 1
                     else:
-                        loc += 1
+                        sens += 1
                     cptt += 1
                 acad.app.ZoomExtents()
                 doc.SaveAs(fil[:-3] + 'dwg')
                 time.sleep(2)
                 doc.close()
                 time.sleep(4)
+                # Reconnect to Autocad in the drawing1.
                 acad = Autocad()            
                 doc = acad.ActiveDocument
                 cpt += 1
-            else: # Add 1 to the ignored
+            else: # Add 1 to the ignored.
                 cptI += 1
-        # Final message
+        # Final message.
         acad.prompt(f'\n {str(cpt)} .las processed\n {str(cptI)} .las ignored\n')
     else:
         messagebox.showerror(title='Error',
                     message='No file, or abandonment',)
-# Autocad check
+# Autocad check.
 if acad:
     tracePtLas(acad)
 else:
